2fa9592439c0f110b092393b8d3a7b7f362c82ef
Amlan Alok <amlanalok@gmail.com>
2022-02-19 18:54:09 -0600

    added results in comments

--- python/homework/hw-1/Q2_C.py
+++ python/homework/hw-1/Q2_C.py
@@ -10,3 +10,12 @@ result = leave_one_out(input_data, k_list)
 result = result_accuracy(result, k_list)
 
 print('END')
+
+'''
+The Accuracy values are as follows:
+k = 1, 68.33 %
+k = 3, 73.33 %
+k = 5, 75.00 %
+
+Hence, best performance is for k = 5  
+'''
