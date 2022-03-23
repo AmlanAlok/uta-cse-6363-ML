12cf29c814174ceee932384ad538edad705b7520
Amlan Alok <amlanalok@gmail.com>
2022-03-23 03:46:53 -0500

    accuracy formula changed

--- python/Q3_C.py
+++ python/Q3_C.py
@@ -22,26 +22,35 @@ def fetch_data(file_name):
 
 
 '''Uses all Data'''
-def separate_input_output(input_data):
+def separate_input_output(input_data, leave_pos):
 
     td = np.array(input_data)
 
-    height_data_points = td[:, 0]
+    h_test = td[leave_pos, 0]
+    w_test = td[leave_pos, 1]
+    a_test = td[leave_pos, 2]
+
+    height_data_points = np.concatenate((td[:leave_pos, 0], td[leave_pos+1:, 0]))
     height_data = height_data_points.reshape(height_data_points.shape[0], 1)
     height_data = height_data.astype('float64')
 
-    weight_data_points = td[:, 1]
+    weight_data_points = np.concatenate((td[:leave_pos, 1], td[leave_pos+1:, 1]))
     weight_data = weight_data_points.reshape(weight_data_points.shape[0], 1)
     weight_data = weight_data.astype('float64')
 
-    age_data_points = td[:, 2]
+    age_data_points = np.concatenate((td[:leave_pos, 2], td[leave_pos+1:, 2]))
     age_data = age_data_points.reshape(age_data_points.shape[0], 1)
     age_data = age_data.astype('int64')
 
+    # y_data_points = np.concatenate((td[:leave_pos, 3], td[leave_pos+1:, 3]))
     y_data_points = td[:, 3]
     y_data = y_data_points.reshape(y_data_points.shape[0], 1)
+    y_data_01 = change_y_data(y_data)
+    # y_data_01_leave_out = np.concatenate(y_data_01[:leave_pos], y_data_01[leave_pos+1:])
+    y_true = y_data_01[leave_pos]
+    y_data_01_leave_out = np.delete(y_data_01, leave_pos)
 
-    return height_data, weight_data, age_data, y_data
+    return height_data, weight_data, age_data, y_data_01_leave_out, h_test, w_test, a_test, y_true
 
 
 def get_feature_matrix(height_data, weight_data, age_data):
@@ -73,14 +82,10 @@ def prediction(parameter_matrix, feature_matrix):
     return 0
 
 
-def train(alpha, iterations):
+def train(alpha, iterations, height_data, weight_data, age_data, y_data_01):
 
-    filename = '../datasets/Q3_data.txt'
-    input_data = fetch_data(filename)
-
-    height_data, weight_data, age_data, y_data = separate_input_output(input_data)
     parameter_matrix = get_random_parameter_matrix()
-    y_data_01 = change_y_data(y_data)
+    # y_data_01 = change_y_data(y_data)
 
     for k in range(iterations):
 
@@ -100,19 +105,10 @@ def train(alpha, iterations):
             parameter_matrix = parameter_matrix - partial_derivative
 
         error_np = np.array(error_array)
-        accuracy = 100 - (np.sum(np.square(error_np)) / error_np.size) * 100
-        print('Itr =', k, ' accuracy =', accuracy)
+        accuracy = 100 - (np.sum(np.square(error_np))/error_np.size)*100
+        # print('Itr =', k, ' accuracy =', accuracy)
 
-    print(parameter_matrix)
-
-    ax = plt.axes(projection='3d')
-
-    ax.scatter3D(height_data, weight_data, age_data)
-
-    ax.set_xlabel('Height')
-    ax.set_ylabel('Weight')
-    ax.set_zlabel('Age')
-    plt.show()
+    # print(parameter_matrix)
 
     return parameter_matrix
 
@@ -121,9 +117,28 @@ def main():
 
     print('program started')
     alpha = 0.01
-    iterations = 20
-    parameter_matrix = train(alpha, iterations)
+    iterations = 1
 
+    filename = '../datasets/Q3_data.txt'
+    input_data = fetch_data(filename)
+    error_array = []
+
+    for i in range(len(input_data)):
+
+        height_data, weight_data, age_data, y_data_01, h_test, w_test, a_test, y_true = separate_input_output(input_data, i)  # put i here
+
+        parameter_matrix = train(alpha, iterations, height_data, weight_data, age_data, y_data_01)
+
+        feature_matrix = get_feature_matrix(h_test, w_test, a_test)
+        # print('h,w,a =', h_test, w_test, a_test)
+        y_prediction = prediction(parameter_matrix, feature_matrix)
+
+        err = y_prediction - y_true
+        error_array.append(err)
+
+    error_np = np.array(error_array)
+    accuracy = 100 - (np.sum(np.square(error_np)) / error_np.size) * 100
+    print('Leave one out Accuracy = ', accuracy, ' %')
     print('program ended')
 
 
