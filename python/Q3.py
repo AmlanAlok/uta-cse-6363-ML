b0f84bb5dd96da759328f66bba3470c78947b5f6
Amlan Alok <amlanalok@gmail.com>
2022-03-22 23:23:34 -0500

    Q3a done

---
+++ python/Q3.py
@@ -0,0 +1,161 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def clean_data(line):
+    return line.replace('(', '').replace(')', '').replace(' ', '').strip().split(',')
+
+
+def fetch_data(file_name):
+    # print('inside func '+ inspect.stack()[0][3])
+
+    with open(file_name, 'r') as f:
+        input_data = f.readlines()
+        # print(type(input_data ))
+        # print('Number of data points =', len(input_data ))
+
+        clean_input = list(map(clean_data, input_data))
+
+        f.close()
+
+    return clean_input
+
+
+'''Uses all Data'''
+def separate_input_output(input_data):
+
+    td = np.array(input_data)
+
+    height_data_points = td[:, 0]
+    height_data = height_data_points.reshape(height_data_points.shape[0], 1)
+    height_data = height_data.astype('float64')
+
+    weight_data_points = td[:, 1]
+    weight_data = weight_data_points.reshape(weight_data_points.shape[0], 1)
+    weight_data = weight_data.astype('float64')
+
+    age_data_points = td[:, 2]
+    age_data = age_data_points.reshape(age_data_points.shape[0], 1)
+    age_data = age_data.astype('int64')
+
+    y_data_points = td[:, 3]
+    y_data = y_data_points.reshape(y_data_points.shape[0], 1)
+
+    return height_data, weight_data, age_data, y_data
+
+
+def get_feature_matrix(height_data, weight_data, age_data):
+
+    # ones = np.ones((height_data.shape[0], 1))
+    # fm = np.concatenate((ones, height_data), axis=1)
+    # fm = np.concatenate((fm, weight_data), axis=1)
+    # fm = np.concatenate((fm, age_data), axis=1)
+    #
+    # return np.transpose(fm)
+
+    return np.array([1, height_data, weight_data, age_data], dtype='float64').reshape(4, 1)
+
+
+def get_random_parameter_matrix():
+    return np.random.randn(4, 1)
+    # return np.random.randint(0, 10, (4, 1))
+    # return np.random.rand(0,10,(4, 1))
+
+
+def change_y_data(y_data):
+    value_01 = np.unique(y_data, return_inverse=True)[1]
+    # y = value_01.reshape(1, y_data.shape[0])
+    # return y
+    return value_01
+
+def sigmoid(x):
+
+    return 1/(1+np.exp(-1*x))
+
+def prediction(parameter_matrix, feature_matrix):
+
+    linear_regression_output = np.matmul(np.transpose(parameter_matrix), feature_matrix)
+
+    sigmoid_output = sigmoid(linear_regression_output)
+
+    if sigmoid_output >= 0.5:
+        return 1
+    return 0
+
+
+def train(alpha, iterations):
+
+    filename = '../datasets/Q3_data.txt'
+    input_data = fetch_data(filename)
+
+    height_data, weight_data, age_data, y_data = separate_input_output(input_data)
+    parameter_matrix = get_random_parameter_matrix()
+    y_data_01 = change_y_data(y_data)
+
+    for k in range(iterations):
+
+        error_array = []
+
+        for i in range(height_data.shape[0]):
+
+            feature_matrix = get_feature_matrix(height_data[i], weight_data[i], age_data[i])
+
+            y_prediction = prediction(parameter_matrix, feature_matrix)
+
+            err = y_prediction - y_data_01[i]
+            error_array.append(err)
+
+            partial_derivative = alpha * err * feature_matrix
+
+            parameter_matrix = parameter_matrix - partial_derivative
+
+        error_np = np.array(error_array)
+        mse = (np.sum(np.square(error_np))/error_np.size)*100
+        print('Itr =', k, ' mse =', mse)
+        pass
+
+
+    pass
+    print(parameter_matrix)
+
+
+
+
+
+def main():
+
+    print('program started')
+    alpha = 0.01
+    iterations = 100
+    train(alpha, iterations)
+
+
+    print('program ended')
+
+
+# def train():
+#
+#     filename = '../datasets/Q3_data.txt'
+#     input_data = fetch_data(filename)
+#
+#     height_data, weight_data, age_data, y_data = separate_input_output(input_data)
+#
+#     feature_matrix = get_feature_matrix(height_data, weight_data, age_data)
+#
+#     parameter_matrix = get_random_parameter_matrix()
+#
+#     hx = np.matmul(parameter_matrix, feature_matrix)
+#
+#     y_data_01 = change_y_data(y_data)
+#
+#     err = hx - y_data_01
+#     ''' moore-penrose pseudoinverse numpy '''
+#     pseudo_inv = np.linalg.pinv(feature_matrix)
+#
+#     pp = err * feature_matrix
+#     # parameter_matrix = np.matmul(pseudo_inv, y_data)
+
+if __name__ == "__main__":
+    main()
+
+
